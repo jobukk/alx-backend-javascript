1b8affcd5ff277759f4ae1abf7f4c9c4202bab4e
JobKoech610 <jobkoech610@gmail.com>
2024-08-06 01:18:54 +0300

    101-iterateThroughObject.js

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,11 @@
+export default function iterateThroughObject(reportWithIterator) {
+    // Collect all employee names into an array
+    const employeeNames = [];
+    for (const employee of reportWithIterator) {
+      employeeNames.push(employee);
+    }
+  
+    // Join the names with ' | ' and return the result
+    return employeeNames.join(' | ');
+  }
+  
